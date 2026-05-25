d8276fb813135def958cd40bf9c07cc80eebdcb5
Pyro <156850594+PhyroDude@users.noreply.github.com>
2026-05-25 12:15:34 -0600

    Corrección del error
    Validación final de  la corrección de error junto a un mensaje final

--- CD.py
+++ CD.py
@@ -2,10 +2,12 @@ import matplotlib as mtp
 import numpy as np
 import pandas as pd
 
-print(Hello World!)
+print("Hello World!")
 data = {
   "calories": [420, 380, 390],
   "duration": [50, 40, 45]
 }
 
-myvar = pd.DataFrame data
+myvar = pd.DataFrame(data)
+
+print("Error solucionado!")
